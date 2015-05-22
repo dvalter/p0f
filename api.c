74edec89195f080656d5cb9df315854c50cd10b9
Julian Haupt <jh@spielmeister.com>
2015-05-23 00:25:47 +0200

    new api wip

--- api.c
+++ api.c
@@ -12,6 +12,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <netinet/in.h>
 
@@ -27,14 +28,57 @@
 
 /* Process API queries. */
 
-const char http_200_response[] = "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Type: text/html\r\n\r\n";
+const char http_200_response[] = "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Type: application/json\r\n\r\n";
 const char http_404_response[] = "HTTP/1.1 404 Not Found\r\nConnection: close\r\nContent-Type: text/html\r\n\r\nNot Found";
 const char http_500_response[] = "HTTP/1.1 500 Internal Server Error\r\nConnection: close\r\nContent-Type: text/html\r\n\r\n";
 const char http_501_response[] = "HTTP/1.1 501 Not Implemented\r\nConnection: close\r\nContent-Type: text/html\r\n\r\nNot Implemented";
 
-u8* append(u8* output, u8* input) {
-      strncpy((char*)output, (char*)input, strlen(input));
-      return output + strlen(input);
+u8* append(u8* r, u8* input) {
+      strncpy((char*)r, (char*)input, strlen(input));
+      return r + strlen(input);
+}
+
+
+u8* append_json_u8(u8* r, u8* key, u8 value) {
+  u8 string_value[sizeof(u8)*8+1];
+  sprintf(string_value, "%u", value);
+  r = append(r, "  \"");
+  r = append(r, key);
+  r = append(r, "\": ");
+  r = append(r, string_value);
+  r = append(r, ",\n");
+  return r;
+}
+
+u8* append_json_u32(u8* r, u8* key, u32 value) {
+  u8 string_value[sizeof(u32)*8+1];
+  sprintf(string_value, "%u", value);
+  r = append(r, "  \"");
+  r = append(r, key);
+  r = append(r, "\": ");
+  r = append(r, string_value);
+  r = append(r, ",\n");
+  return r;
+}
+
+u8* append_json_s32(u8* r, u8* key, s32 value) {
+  u8 string_value[sizeof(s32)*8+1];
+  sprintf(string_value, "%d", value);
+  r = append(r, "  \"");
+  r = append(r, key);
+  r = append(r, "\": ");
+  r = append(r, string_value);
+  r = append(r, ",\n");
+  return r;
+}
+
+u8* append_json_string(u8* r, u8* key, u8* value) {
+    r = append(r, "  \"");
+    r = append(r, key);
+    r = append(r, "\": \"");
+    r = append(r, value);
+    r = append(r, "\",\n");
+    return r;
 }
 
 u8 handle_query(u8* q, u8* r) {
@@ -88,8 +132,8 @@ u8 handle_query(u8* q, u8* r) {
 
   // we only support / as uri
   if (strcasecmp(uri, "/")) {
-    strncpy((char*)r, http_404_response, sizeof(http_404_response)-1);
-    return (http_404_response)-1;
+    r = append(r, http_404_response);
+    return r - response_start;
   }
 
   //extract parameter
@@ -115,21 +159,17 @@ u8 handle_query(u8* q, u8* r) {
 
   DEBUG("[X] API Request for ip_version: %s, ip: %s\n", param_ip_version, param_ip);
 
-  if (!strcasecmp(param_ip_version, "4")) {
-    ip_ver = IP_VER4;
+  if (strcmp(param_ip_version, "4") == 0 ||
+      strcmp(param_ip_version, "6") == 0 ) {
 
-    if (inet_pton(AF_INET, param_ip, cli_addr) <= 0) {
+    ip_ver = (strcmp(param_ip_version, "4") == 0) ? IP_VER4 : IP_VER6;
+
+    if (inet_pton(
+        (ip_ver == IP_VER4) ? AF_INET : AF_INET6,
+        param_ip,
+        cli_addr) <= 0) {
       r = append(r, http_500_response);
-      r = append(r, "Could not parse IPv4 address from ip parameter");
-     return r - response_start;
-    }
-
-  } else if (!strcasecmp(param_ip_version, "6")) {
-    ip_ver = IP_VER6;
-
-    if (inet_pton(AF_INET6, param_ip, cli_addr) <= 0) {
-      r = append(r, http_500_response);
-      r = append(r, "Could not parse IPv6 address from ip parameter");
+      r = append(r, "Could not parse ip address from ip parameter");
       return r - response_start;
     }
 
@@ -147,23 +187,31 @@ u8 handle_query(u8* q, u8* r) {
 
   } else {
     r = append(r, http_200_response);
+    r = append(r, "{\n");
+
+    r = append_json_u8      (r, "ip_version",       ip_ver);
+    r = append_json_string  (r, "ip",               addr_to_str(cli_addr, ip_ver));
+
+    r = append_json_s32     (r, "uptime_minutes",   h->last_up_min);
+
+//    r->up_mod_days      = h->up_mod_days;
+
+
+    r = append_json_u32     (r, "first_seen",       h->first_seen);
+    r = append_json_u32     (r, "last_seen",        h->last_seen);
+    r = append_json_u32     (r, "total_conn",       h->total_conn);
+
+    r = append_json_u8    (r, "distance",     h->distance);
+  //  r = append_json_string(r, "http_raw_sig", h->http_raw_sig);
+
+
+    r = append(r, "}\n");
+
+
   }
 
-
-
-
   return r - response_start;
-
-
-
-//  response_length += append(r, h->ssl_raw_sig);
-
-
 /*
-
-  r->first_seen = h->first_seen;
-  r->last_seen  = h->last_seen;
-  r->total_conn = h->total_conn;
 
   if (h->last_name_id != -1) {
 
@@ -191,23 +239,19 @@ u8 handle_query(u8* q, u8* r) {
   if (h->language)
     strncpy((char*)r->language, (char*)h->language, P0F_STR_MAX + 1);
 
-  r->bad_sw           = h->bad_sw;
   r->last_nat         = h->last_nat;
   r->last_chg         = h->last_chg;
-  r->up_mod_days      = h->up_mod_days;
-  r->distance         = h->distance;
+
+
+
+
+
   r->os_match_q       = h->last_quality;
 
-  if (h->http_raw_sig)
-    strncpy((char*)r->http_raw_sig, (char*)h->http_raw_sig, HTTP_MAX_SHOW + 1);
+
 
   r->ssl_remote_time  = h->ssl_remote_time;
   r->ssl_recv_time    = h->ssl_recv_time;
 
-
-
-  if (h->last_up_min != -1) r->uptime_min = h->last_up_min;
 */
-
-  return 0;
 }
