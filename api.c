3fa9e3f6d53abcc24e5efff283fa773543fd4fda
Julian Haupt <julian.haupt@hauptmedia.de>
2015-05-24 22:39:19 +0200

    fixed name of http_signature

--- api.c
+++ api.c
@@ -191,7 +191,7 @@ s32 handle_query(u8* q, u8* r) {
     r = append_json_u32     (r, "distance",          h->distance);
     r = append_json_u32     (r, "mtu",               h->mtu);
 
-    r = append_json_string  (r, "http_raw_sig",      &h->http_raw_sig);
+    r = append_json_string  (r, "http_signature",    &h->http_signature);
 
     r = append_json_string  (r, "ssl_signature",     &h->ssl_signature);
     r = append_json_u32     (r, "ssl_remote_time",   h->ssl_remote_time);
