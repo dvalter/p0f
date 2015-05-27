28787e4311e8078cb17278e4ebad68aef9259935
Julian Haupt <julian.haupt@hauptmedia.de>
2015-05-27 18:25:11 +0200

    moved mtu database to php scope

---
+++ php/mtu.php
@@ -0,0 +1,38 @@
+<?php
+return array(
+    256 => 'AX.25 radio modem',
+    552 => 'SLIP',
+
+    576     => 'Ethernet or modem',
+    1500    => 'Ethernet or modem',
+
+    1240    => 'GIF',
+    1280    => 'GIF',
+
+    1300    => 'generic tunnel or VPN',
+    1400    => 'generic tunnel or VPN',
+    1420    => 'generic tunnel or VPN',
+    1440    => 'generic tunnel or VPN',
+    1450    => 'generic tunnel or VPN',
+    1460    => 'generic tunnel or VPN',
+
+    1476    => 'IPSec or GRE',
+
+    1480    => 'IPIP or SIT',
+
+    1490    => 'PPTP',
+
+    1452    => 'DSL',
+    1454    => 'DSL',
+    1492    => 'DSL',
+
+    1496    => 'VLAN',
+
+    1656    => 'Ericsson HIS modem',
+
+    9000    => 'Jumbo Ethernet',
+
+    3924     => 'loopback',
+    16384    => 'loopback',
+    16436    => 'loopback',
+);
--- php/tcp_signatures.php
+++ php/tcp_signatures.php
@@ -1,14 +1,12 @@
 <?php
 return array(
     '4:64:mss,sok,ts,nop,ws:df,id+'                  => 'Linux',
-    '4:64:mss,nop,nop,sok,nop,ws:df,id+'             => 'Linux',
-    '4:64:mss:df,id+'                                => 'Linux',
 
     '4:128:mss,nop,nop,sok:df,id+'                   => 'Windows NT, XP, 7 or 8',
     '4:128:mss,nop,ws,nop,nop,sok:df,id+'            => 'Windows NT, XP, 7 or 8',
     '4:128:mss,nop,ws,sok,ts:df,id+'                 => 'Windows NT, XP, 7 or 8',
 
-    '4:64:mss,nop,ws,nop,nop,ts,sok,eol+1:df,id+'   => 'MacOS X 10.9 or newer (sometimes iPhone or iPad)',
+    '4:64:mss,nop,ws,nop,nop,ts,sok,eol+1:df,id+'   => 'MacOS X / iPhone / iPad',
 
     '4:64:mss,nop,ws,sok,ts:df,id+'                 => 'FreeBSD',
 
