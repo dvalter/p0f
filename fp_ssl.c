cad8ea5387cfd09a8a30cc960ed639701448fc0d
Dmitry Valter <dvalter@protonmail.com>
2022-05-22 18:43:25 +0000

    Add tls

--- fp_ssl.c
+++ fp_ssl.c
@@ -56,6 +56,369 @@ static struct ssl_sig_record* signatures;
 static u32 signatures_cnt;
 
 
+/*
+ * tls-hello-dump.c
+ *
+ * TLS ClientHello/ServerHello Dumper (for XMPP).
+ *
+ * Version 0.5 (2013-11-06)
+ * Copyright (C) 2013 Georg Lukas <georg@op-co.de>
+ *
+ ****************************************************************************
+ *
+ * This software is a modification of:
+ *
+ * sniffex.c
+ *
+ * Sniffer example of TCP/IP packet capture using libpcap.
+ * 
+ * Version 0.1.1 (2005-07-05)
+ * Copyright (c) 2005 The Tcpdump Group
+ *
+ * This software is intended to be used as a practical example and 
+ * demonstration of the libpcap library; available at:
+ * http://www.tcpdump.org/
+ *
+ ****************************************************************************
+ *
+ * This software is a modification of Tim Carstens' "sniffer.c"
+ * demonstration source code, released as follows:
+ * 
+ * sniffer.c
+ * Copyright (c) 2002 Tim Carstens
+ * 2002-01-07
+ * Demonstration of using libpcap
+ * timcarst -at- yahoo -dot- com
+ * 
+ * "sniffer.c" is distributed under these terms:
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 4. The name "Tim Carstens" may not be used to endorse or promote
+ *    products derived from this software without prior written permission
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * <end of "sniffer.c" terms>
+ *
+ * This software, "sniffex.c", is a derivative work of "sniffer.c" and is
+ * covered by the following terms:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Because this is a derivative work, you must comply with the "sniffer.c"
+ *    terms reproduced above.
+ * 2. Redistributions of source code must retain the Tcpdump Group copyright
+ *    notice at the top of this source file, this list of conditions and the
+ *    following disclaimer.
+ * 3. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 4. The names "tcpdump" or "libpcap" may not be used to endorse or promote
+ *    products derived from this software without prior written permission.
+ *
+ * THERE IS ABSOLUTELY NO WARRANTY FOR THIS PROGRAM.
+ * BECAUSE THE PROGRAM IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
+ * FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW.  EXCEPT WHEN
+ * OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
+ * PROVIDE THE PROGRAM "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED
+ * OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  THE ENTIRE RISK AS
+ * TO THE QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU.  SHOULD THE
+ * PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING,
+ * REPAIR OR CORRECTION.
+ * 
+ * IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
+ * WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
+ * REDISTRIBUTE THE PROGRAM AS PERMITTED ABOVE, BE LIABLE TO YOU FOR DAMAGES,
+ * INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING
+ * OUT OF THE USE OR INABILITY TO USE THE PROGRAM (INCLUDING BUT NOT LIMITED
+ * TO LOSS OF DATA OR DATA BEING RENDERED INACCURATE OR LOSSES SUSTAINED BY
+ * YOU OR THIRD PARTIES OR A FAILURE OF THE PROGRAM TO OPERATE WITH ANY OTHER
+ * PROGRAMS), EVEN IF SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGES.
+ * <end of "sniffex.c" terms>
+ * 
+ ****************************************************************************
+ *
+ * Below is an excerpt from an email from Guy Harris on the tcpdump-workers
+ * mail list when someone asked, "How do I get the length of the TCP
+ * payload?" Guy Harris' slightly snipped response (edited by him to
+ * speak of the IPv4 header length and TCP data offset without referring
+ * to bitfield structure members) is reproduced below:
+ * 
+ * The Ethernet size is always 14 bytes.
+ * 
+ * <snip>...</snip>
+ *
+ * In fact, you *MUST* assume the Ethernet header is 14 bytes, *and*, if 
+ * you're using structures, you must use structures where the members 
+ * always have the same size on all platforms, because the sizes of the 
+ * fields in Ethernet - and IP, and TCP, and... - headers are defined by 
+ * the protocol specification, not by the way a particular platform's C 
+ * compiler works.)
+ *
+ * The IP header size, in bytes, is the value of the IP header length,
+ * as extracted from the "ip_vhl" field of "struct sniff_ip" with
+ * the "IP_HL()" macro, times 4 ("times 4" because it's in units of
+ * 4-byte words).  If that value is less than 20 - i.e., if the value
+ * extracted with "IP_HL()" is less than 5 - you have a malformed
+ * IP datagram.
+ *
+ * The TCP header size, in bytes, is the value of the TCP data offset,
+ * as extracted from the "th_offx2" field of "struct sniff_tcp" with
+ * the "TH_OFF()" macro, times 4 (for the same reason - 4-byte words).
+ * If that value is less than 20 - i.e., if the value extracted with
+ * "TH_OFF()" is less than 5 - you have a malformed TCP segment.
+ *
+ * So, to find the IP header in an Ethernet packet, look 14 bytes after 
+ * the beginning of the packet data.  To find the TCP header, look 
+ * "IP_HL(ip)*4" bytes after the beginning of the IP header.  To find the
+ * TCP payload, look "TH_OFF(tcp)*4" bytes after the beginning of the TCP
+ * header.
+ * 
+ * To find out how much payload there is:
+ *
+ * Take the IP *total* length field - "ip_len" in "struct sniff_ip" 
+ * - and, first, check whether it's less than "IP_HL(ip)*4" (after
+ * you've checked whether "IP_HL(ip)" is >= 5).  If it is, you have
+ * a malformed IP datagram.
+ *
+ * Otherwise, subtract "IP_HL(ip)*4" from it; that gives you the length
+ * of the TCP segment, including the TCP header.  If that's less than
+ * "TH_OFF(tcp)*4" (after you've checked whether "TH_OFF(tcp)" is >= 5),
+ * you have a malformed TCP segment.
+ *
+ * Otherwise, subtract "TH_OFF(tcp)*4" from it; that gives you the
+ * length of the TCP payload.
+ *
+ * Note that you also need to make sure that you don't go past the end 
+ * of the captured data in the packet - you might, for example, have a 
+ * 15-byte Ethernet packet that claims to contain an IP datagram, but if 
+ * it's 15 bytes, it has only one byte of Ethernet payload, which is too 
+ * small for an IP header.  The length of the captured data is given in 
+ * the "caplen" field in the "struct pcap_pkthdr"; it might be less than 
+ * the length of the packet, if you're capturing with a snapshot length 
+ * other than a value >= the maximum packet size.
+ * <end of response>
+ * 
+ ****************************************************************************
+ * 
+ * Example compiler command-line for GCC:
+ *   gcc -Wall -o sniffex sniffex.c -lpcap
+ * 
+ ****************************************************************************
+ *
+ * Code Comments
+ *
+ * This section contains additional information and explanations regarding
+ * comments in the source code. It serves as documentaion and rationale
+ * for why the code is written as it is without hindering readability, as it
+ * might if it were placed along with the actual code inline. References in
+ * the code appear as footnote notation (e.g. [1]).
+ *
+ * 1. Ethernet headers are always exactly 14 bytes, so we define this
+ * explicitly with "#define". Since some compilers might pad structures to a
+ * multiple of 4 bytes - some versions of GCC for ARM may do this -
+ * "sizeof (struct sniff_ethernet)" isn't used.
+ * 
+ * 2. Check the link-layer type of the device that's being opened to make
+ * sure it's Ethernet, since that's all we handle in this example. Other
+ * link-layer types may have different length headers (see [1]).
+ *
+ * 3. This is the filter expression that tells libpcap which packets we're
+ * interested in (i.e. which packets to capture). Since this source example
+ * focuses on IP and TCP, we use the expression "ip", so we know we'll only
+ * encounter IP packets. The capture filter syntax, along with some
+ * examples, is documented in the tcpdump man page under "expression."
+ * Below are a few simple examples:
+ *
+ * Expression     Description
+ * ----------     -----------
+ * ip         Capture all IP packets.
+ * tcp          Capture only TCP packets.
+ * tcp port 80      Capture only TCP packets with a port equal to 80.
+ * ip host 10.1.2.3   Capture all IP packets to or from host 10.1.2.3.
+ *
+ ****************************************************************************
+ *
+ */
+
+#include <pwd.h>
+#include <pcap.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define SSL_MIN_GOOD_VERSION  0x002
+#define SSL_MAX_GOOD_VERSION  0x304 // let's be optimistic here!
+
+#define TLS_HANDSHAKE   22
+#define TLS_CLIENT_HELLO  1
+#define TLS_SERVER_HELLO  2
+
+#define OFFSET_HELLO_VERSION  9
+#define OFFSET_SESSION_LENGTH 43
+#define OFFSET_CIPHER_LIST  44
+
+// for fingerprint
+#define TLS_EXT_MAX 64
+
+char*
+ssl_version(u_short version) {
+  static char hex[7];
+  switch (version) {
+    case 0x002: return "SSLv2";
+    case 0x300: return "SSLv3";
+    case 0x301: return "TLSv1";
+    case 0x302: return "TLSv1.1";
+    case 0x303: return "TLSv1.2";
+  }
+  snprintf(hex, sizeof(hex), "0x%04hx", version);
+  return hex;
+}
+
+/* Is u32 list of ciphers/extensions matching the signature?
+   first argument is record (star and question mark allowed),
+   second one is an exact signature. */
+
+/* Unpack TLS fragment to a signature. We expect to hear ClientHello
+ message.  -1 on parsing error, 1 if signature was extracted. */
+
+static int fingerprint_tls(struct ssl_sig* sig, const u8* fragment,
+                              u32 frag_len, u16 record_version, u32 local_time) {
+  const u_char *payload;                  /* Packet payload */
+
+  int size_ip;
+  int size_iptotal;
+  int size_tcp;
+  int size_payload = frag_len;
+
+  /* define/compute tcp payload (segment) offset */
+  payload = (u_char *)(fragment);
+
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 8; j++) {
+      DEBUG("%02x ", payload[8*i + j]);
+    }
+    DEBUG(" ");
+  }
+  DEBUG("\n");
+
+  if (payload[0] != TLS_HANDSHAKE) {
+    DEBUG("Not a TLS handshake: 0x%02hhx\n", payload[0]);
+    return -1;
+  }
+
+  u_short proto_version = payload[1]*256 + payload[2];
+  DEBUG("%s ", ssl_version(proto_version));
+  u_short hello_version = payload[OFFSET_HELLO_VERSION]*256 + payload[OFFSET_HELLO_VERSION+1];
+
+  if (proto_version < SSL_MIN_GOOD_VERSION || proto_version >= SSL_MAX_GOOD_VERSION ||
+      hello_version < SSL_MIN_GOOD_VERSION || hello_version >= SSL_MAX_GOOD_VERSION) {
+    DEBUG("%s bad version(s)\n", ssl_version(hello_version));
+    return -1;
+  }
+
+  sig->request_version = hello_version;
+
+  if (sig->request_version != record_version) {
+    sig->flags |= SSL_FLAG_VER;
+  }
+
+  // skip session ID
+  const u_char *cipher_data = &payload[OFFSET_SESSION_LENGTH];
+  if (size_payload < OFFSET_SESSION_LENGTH + cipher_data[0] + 3) {
+    DEBUG("SessionID too long: %hhu bytes (max %hhu)\n", cipher_data[0], OFFSET_SESSION_LENGTH + cipher_data[0] + 3);
+    return -1;
+  }
+
+  sig->remote_time = 0;
+  sig->recv_time = 0;
+
+  cipher_data += 1 + cipher_data[0];
+
+  u_short cs_len;
+
+  switch (payload[5]) {
+    case TLS_CLIENT_HELLO:
+      cs_len = cipher_data[0]*256 + cipher_data[1];
+      sig->cipher_suites = ck_alloc(((cs_len / 2) + 1) * sizeof(u32));
+      cipher_data += 2; // skip cipher suites length
+      // FIXME: check for buffer overruns
+      int cs_id;
+      for (cs_id = 0; cs_id < cs_len/2; cs_id++) {
+        sig->cipher_suites[cs_id] = (cipher_data[2*cs_id] << 8) | cipher_data[2*cs_id + 1];
+        DEBUG(":%02hhX%02hhX", cipher_data[2*cs_id], cipher_data[2*cs_id + 1]);
+      }
+      sig->cipher_suites[cs_len/2] = END_MARKER;
+      DEBUG(":\n");
+      break;
+    default:
+      DEBUG("Not a ClientHello\n");
+      return -1;
+  }
+
+
+  // skip copression
+  const u_char *extension_data = &cipher_data[cs_len];
+
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 8; j++) {
+      DEBUG("%02x ", extension_data [8*i + j]);
+    }
+    DEBUG(" ");
+  }
+  DEBUG("\n");
+
+  extension_data = extension_data + 1 + extension_data[0];
+  u_short extensions_len = (extension_data[0] << 8) + extension_data[1];
+  extension_data += 2;
+
+  u_short offset = 0;
+  u32 ext_capacity = 5;
+  u_short extension_count = 0;
+  DEBUG("extensions len %u ", extensions_len);
+  sig->extensions = ck_alloc(TLS_EXT_MAX + 1);
+  while(offset < extensions_len) {
+      if (extension_count >= TLS_EXT_MAX)
+        break;
+      sig->extensions[extension_count] = (extension_data[offset] << 8) + extension_data[offset + 1];
+      u_short len = (extension_data[offset + 2] << 8) + extension_data[offset + 3];
+      DEBUG(":%02hhX%02hhX (+%u)", (extension_data[offset] << 8), extension_data[offset + 1], len);
+      offset += len + 4;      
+      extension_count += 1;
+  }
+  DEBUG(":\n");
+  sig->extensions[extension_count] = END_MARKER;
+
+  return 1;
+}
+
+
 /* Decode a string of comma separated hex numbers into an annotated
    u32 array. Exit with success on '\0' or ':'. */
 
@@ -517,15 +880,23 @@ u8 process_ssl(u8 to_srv, struct packet_flow* f) {
 
     u8* fragment = f->request + sizeof(struct ssl3_record_hdr);
 
-    success = fingerprint_ssl_v3(&sig, fragment, fragment_len,
-                                 record_version,
-                                 f->client->last_seen);
+    if (record_version >= 0x301) {
+      success = fingerprint_tls(&sig, f->request, fragment_len + sizeof(struct ssl3_record_hdr),
+                                record_version,
+                                f->client->last_seen);
+    } else {
+      success = fingerprint_ssl_v3(&sig, fragment, fragment_len,
+                                   record_version,
+                                   f->client->last_seen);
+    }
+
+    
 
   }
 
   if (success != 1) {
 
-    DEBUG("[#] Does not look like SSLv3.\n");
+    DEBUG("[#] Does not look like SSLv3/TLS.\n");
 
     f->in_ssl = -1;
     return 0;
